ae2f42d31e5c482cfc303d389e9a6ae9bc2789e4
Utkarsh Kant <utkarsh.kant@gmail.com>
2026-01-04 14:15:14 +0530

    added load file functioanlity

--- utils.py
+++ utils.py
@@ -3,6 +3,12 @@ import re
 import json
 from pathlib import Path
 
+def load_file(file_path: str) -> str:
+    """
+    Load and return file contents as a string.
+    """
+    return Path(file_path).read_text().strip()
+
 def get_logger(name: str) -> logging.Logger:
     """
     Sets up and returns a logger with the specified name.
